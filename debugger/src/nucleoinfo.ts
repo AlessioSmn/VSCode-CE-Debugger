93b14cb473e1a719595adcd510d9cb35f7b72411
alessio.smn <77299123+AlessioSmn@users.noreply.github.com>
2025-02-17 15:24:25 +0100

    refactor: if changed to switch

--- debugger/src/nucleoinfo.ts
+++ debugger/src/nucleoinfo.ts
@@ -37,28 +37,32 @@ export class NucleoInfo {
 		this._extensionUri = extensionUri;
 		this._panelType = panelType;
 
-		if(panelType == PanelType.Process){
-			this.compileProcessTemplates = ProcessInfoMethods.compileProcessTemplates.bind(this);
-			this.formatEsecuzione = ProcessInfoMethods.formatEsecuzione.bind(this);
-			this.formatCodaPronti = ProcessInfoMethods.formatCodaPronti.bind(this);
-			this.formatCodaSospesi = ProcessInfoMethods.formatCodaSospesi.bind(this);
-			this.formatSemaphoreList = ProcessInfoMethods.formatSemaphoreList.bind(this);
-			this.formatProcesses = ProcessInfoMethods.formatProcesses.bind(this);
+		switch(this._panelType){
+			case PanelType.Process:
+				this.compileProcessTemplates = ProcessInfoMethods.compileProcessTemplates.bind(this);
+				this.formatEsecuzione = ProcessInfoMethods.formatEsecuzione.bind(this);
+				this.formatCodaPronti = ProcessInfoMethods.formatCodaPronti.bind(this);
+				this.formatCodaSospesi = ProcessInfoMethods.formatCodaSospesi.bind(this);
+				this.formatSemaphoreList = ProcessInfoMethods.formatSemaphoreList.bind(this);
+				this.formatProcesses = ProcessInfoMethods.formatProcesses.bind(this);
 
-			// Compiles the Handlebars templates once, at the start of the extension
-			this.templateProcess = this.compileProcessPanelTemplate();
-			this.compileProcessTemplates();
-		}
+				// Compiles the Handlebars templates once, at the start of the extension
+				this.templateProcess = this.compileProcessPanelTemplate();
+				this.compileProcessTemplates();
 
-		if(panelType == PanelType.Memory){
-			this.compileVmTemplates = VmInfoMethods.compileVmTemplates.bind(this);
-			this.formatVmMaps = VmInfoMethods.formatVmMaps.bind(this);
-			this.formatVmTree = VmInfoMethods.formatVmTree.bind(this);
-			// this.vmPathAnalyzer = VmInfoMethods.vmPathAnalyzer.bind(this);
+				break;
 
-			// Compiles the Handlebars templates once, at the start of the extension
-			this.templateVm = this.compileVmPanelTemplate();
-			this.compileVmTemplates();
+			case PanelType.Memory:
+				this.compileVmTemplates = VmInfoMethods.compileVmTemplates.bind(this);
+				this.formatVmMaps = VmInfoMethods.formatVmMaps.bind(this);
+				this.formatVmTree = VmInfoMethods.formatVmTree.bind(this);
+				// this.vmPathAnalyzer = VmInfoMethods.vmPathAnalyzer.bind(this);
+
+				// Compiles the Handlebars templates once, at the start of the extension
+				this.templateVm = this.compileVmPanelTemplate();
+				this.compileVmTemplates();
+
+				break;
 		}
 
 		// Listen for when the panel is disposed: this happens when the user closes the panel
